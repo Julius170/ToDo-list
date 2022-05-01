de69c9a066ffb0470f8ddeecd624dcbf646af671
Julius170 <juliusyoola5@gmail.com>
2022-05-01 13:03:35 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// jhint esversion6
+
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const app = express()
+app.get("/", function(req, res) {
+    // res.send (__dirname + "/index.html");
+
+    var today = new Date();
+    var currentDay = today.getDay()
+    if (currentDay === 1 || currentDay === 6) {
+        res.send ("Yay its the weekend!");
+    }else {
+        res.send("Boo i have to work!")
+    }
+
+});
+
+
+app.listen (3010, function() {
+    console.log("Server is runing on port http://localhost:3010/")
+});
+
+
