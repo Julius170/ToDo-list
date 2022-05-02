fbcbfce02d6cf43c055db5ba8d3cac981300d3e0
Julius170 <juliusyoola5@gmail.com>
2022-05-02 23:58:11 +0100

    adding a control flow tag

--- app.js
+++ app.js
@@ -13,14 +13,33 @@ app.get("/", function(req, res) {
     var today = new Date();
     var currentDay = today.getDay()
     var day = "";
-    if (currentDay === 1 || currentDay === 6) {
-        day = "weekend";
-        res.render ("list", {kindOfDay: day});
-    }else {
-        day = 'weekday'
-        res.render(__dirname + "/weekday.html");
+
+    if (currentDay === 1) {
+        day = "Sunday";
     }
-    
+    else if (currentDay === 2){
+        day = 'Monday'
+    }
+    else if (currentDay === 3){
+        day = 'Tuesday'
+    }
+    else if (currentDay === 4){
+        day = 'Wednesday'
+    }  
+    else if (currentDay === 5){
+        day = 'Thursday'
+    }  
+    else if (currentDay === 6){
+        day = 'Friday'
+    }  
+    else if (currentDay === 7){
+        day = 'Saturday'
+    }
+    else {
+    console.log("Error: current day is " + day)
+    }
+
+    res.render ("list", {kindOfDay: day});
 });
 
 
