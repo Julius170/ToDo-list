f57c9207fd7d73c9f774f5f9584f99c2f6fb184f
Julius170 <juliusyoola5@gmail.com>
2022-05-03 01:01:23 +0100

    completed basic todo list

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
 // jhint esversion6
 
-
 const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
     var today = new Date();
-    var currentDay = today.getDay()
-    var day = "";
 
-    if (currentDay === 1) {
-        day = "Sunday";
-    }
-    else if (currentDay === 2){
-        day = 'Monday'
-    }
-    else if (currentDay === 3){
-        day = 'Tuesday'
-    }
-    else if (currentDay === 4){
-        day = 'Wednesday'
-    }  
-    else if (currentDay === 5){
-        day = 'Thursday'
-    }  
-    else if (currentDay === 6){
-        day = 'Friday'
-    }  
-    else if (currentDay === 7){
-        day = 'Saturday'
-    }
-    else {
-    console.log("Error: current day is " + day)
-    }
+    var options = {
+        weekday:'long',
+        day: 'numeric', 
+        month: 'long',
+    };
+    var day = today.toLocaleDateString('en-US', options)
 
-    res.render ("list", {kindOfDay: day});
+    res.render ("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post('/', (req, res)=>{
+    var item = (req.body.newItem);
+    items.push(item);
+    
+    res.redirect("/");
+
+});
+
+
+
+
 
 
 app.listen (3010, function() {
