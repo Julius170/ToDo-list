cca2e70b6190c533a6c0bb29fa2ea690bc28208b
Julius170 <juliusyoola5@gmail.com>
2022-05-03 02:22:33 +0100

    --- Auto-Git Commit ---

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 
 const app = express();
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set('view engine', "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,20 +22,37 @@ app.get("/", function(req, res) {
     };
     let day = today.toLocaleDateString('en-US', options)
 
-    res.render ("list", {kindOfDay: day, newListItems: items});
+    res.render ("list", {listTitle: day, newListItems: items});
 });
 
 app.post('/', (req, res)=>{
     let item = (req.body.newItem);
-    items.push(item);
-    
-    res.redirect("/");
+
+    if ((req.body.list) === 'work') {
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else {
+        items.push(item); 
+        res.redirect("/");
+        
+    }
+
+
 
 });
 
+app.get("/work", (req, res) => {
+    res.render("list", {listTitle: "Work List", newListItems:workItems});
 
 
+});
 
+app.post("/work", (req, res) => {
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work")
+})
 
 
 app.listen (3010, function() {
