8d3687ab355fcd7da9cb71f270296cb6bccc56a5
Julius170 <juliusyoola5@gmail.com>
2022-05-03 02:03:58 +0100

    implemented the ..css file

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set('view engine', "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get("/", function(req, res) {
     let today = new Date();
